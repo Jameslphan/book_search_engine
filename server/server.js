e0ee5878227cac58e3a841191c57f40195e70d34
Phanimal <jameslphan@hotmail.com>
2022-12-17 14:20:25 -0800

    Jason's Package update

--- server/server.js
+++ server/server.js
@@ -2,9 +2,16 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
